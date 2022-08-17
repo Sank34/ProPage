bccb288a0c1b3227989e9520bb71371f442d5721
Sank <71709931+Sank34@users.noreply.github.com>
2022-08-17 17:45:32 +0300

    system file prepared

---
+++ system.js
@@ -0,0 +1,7 @@
+/* ProPage v1.0.0
+    * ProPage lets you create your own about-you page for free
+    * This is the system file that works behind the website's design
+    * If you wish to change something from here, you can always 
+    * contribuite to the project on github.
+    * Made by Sanke#111 & Mihai.Vue#5199 *
+*/
