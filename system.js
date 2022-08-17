4ae2a449fa339b66f67638a8af3147f4965f093e
Sank <71709931+Sank34@users.noreply.github.com>
2022-08-17 16:38:18 +0300

    minor update

--- system.js
+++ system.js
@@ -2,5 +2,5 @@
     * ProPage Let's you create your own About-you-page
     * This file is the javascript system behind the website
     * If you wish to modify the code from here, you can everytime contribuite to the project
-    * Developed By Sanke#111 & Mihai.Vue 
+    * Developed By Sanke#111 & Mihai.Vue#5199
 */
